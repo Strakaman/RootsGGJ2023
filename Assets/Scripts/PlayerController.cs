68ea2a48a5ea080d1f27c97fd4514f3d642da203
Omari Straker <straka3000@gmail.com>
2023-01-30 21:19:58 -0800

    adding gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,10 @@ public class PlayerController : MonoBehaviour
 {
     private Vector2 moveInput;
     public CharacterController characterController;
+    public InputAction jumpAction;
     public float speed;
+    public float gravity = 9.81f;
+    public float jumpForce;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +23,12 @@ public class PlayerController : MonoBehaviour
         Vector3 moveVector = new Vector3(moveInput.x, 0, moveInput.y) * Time.deltaTime * speed;
         if (characterController.isGrounded)
         {
-            moveVector.y = -1f;
+            //moveVector.y = -1f;
+            
         }
         else
         {
-            moveVector.y -= 10f;
+            moveVector.y -= gravity * 2f * Time.deltaTime;
         }
         characterController.Move(moveVector);
     }
@@ -36,4 +40,5 @@ public class PlayerController : MonoBehaviour
         //transform.Rotate(new Vector3(0, movementVector.y, 0));
         //characterController.Move(new Vector3(movementVector.x, 0, movementVector.y));
     }
+
 }
