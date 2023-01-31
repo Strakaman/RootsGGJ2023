38b00a6f1d06ebdb2365573311a5cbf40b09a245
Omari Straker <straka3000@gmail.com>
2023-01-30 19:55:00 -0800

    project settings and give player basic movement and gravity. camera is very jank

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Vector2 moveInput;
     public CharacterController characterController;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +17,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveVector = new Vector3(moveInput.x, 0, moveInput.y) * Time.deltaTime * speed;
+        if (characterController.isGrounded)
+        {
+            moveVector.y = -1f;
+        }
+        else
+        {
+            moveVector.y -= 10f;
+        }
+        characterController.Move(moveVector);
     }
 
     public void GetMovementInput(InputAction.CallbackContext callbackContext)
     {
-        Vector2 movementVector = callbackContext.ReadValue<Vector2>();
-        Debug.Log("Movement Input: " + movementVector);
+        moveInput = callbackContext.ReadValue<Vector2>();
+        //Debug.Log("Movement Input: " + movementVector);
         //transform.Rotate(new Vector3(0, movementVector.y, 0));
         //characterController.Move(new Vector3(movementVector.x, 0, movementVector.y));
     }
