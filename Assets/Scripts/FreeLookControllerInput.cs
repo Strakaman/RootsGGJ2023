b9d7244837d9a57f80437682bed3d18c498c388f
Omari Straker <straka3000@gmail.com>
2023-01-31 21:03:21 -0800

    Added right analog controller support. Also meta files

---
+++ Assets/Scripts/FreeLookControllerInput.cs
@@ -0,0 +1,41 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CinemachineFreeLook))]
+public class FreeLookControllerInput : MonoBehaviour
+{
+    [Range(0f, 10f)] public float LookSpeed = 1f;
+    public bool InvertY = false;
+    private CinemachineFreeLook _freeLookComponent;
+
+    private Vector2 camMoveInput;
+
+    void Start()
+    {
+        _freeLookComponent = GetComponent<CinemachineFreeLook>();
+    }
+
+    void Update()
+    {
+        //Normalize the vector to have an uniform vector in whichever form it came from (I.E Gamepad, mouse, etc)
+        Vector2 lookMovement = camMoveInput.normalized;
+        lookMovement.y = InvertY ? -lookMovement.y : lookMovement.y;
+
+        // This is because X axis is only contains between -180 and 180 instead of 0 and 1 like the Y axis
+        lookMovement.x = lookMovement.x * 180f;
+
+        //Ajust axis values using look speed and Time.deltaTime so the look doesn't go faster if there is more FPS
+        _freeLookComponent.m_XAxis.Value += lookMovement.x * LookSpeed * Time.deltaTime;
+        _freeLookComponent.m_YAxis.Value += lookMovement.y * LookSpeed * Time.deltaTime;
+    }
+
+    // Update the look movement each time the event is trigger
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        Debug.Log("event man: " + context.ReadValue<Vector2>());
+        camMoveInput = context.ReadValue<Vector2>();
+    }
+}
