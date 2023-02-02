836e55a1e10ca89c36c294e322b2505d614bb6cd
Omari Straker <straka3000@gmail.com>
2023-02-01 20:00:42 -0800

    camera and movement and animations are better

--- Assets/Scripts/FreeLookControllerInput.cs
+++ Assets/Scripts/FreeLookControllerInput.cs
@@ -1,61 +1,61 @@
-using Cinemachine;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem;
+//using Cinemachine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(CinemachineFreeLook))]
-public class FreeLookControllerInput : MonoBehaviour
-{
-    [Range(0f, 10f)] public float LookSpeed = 1f;
-    public bool InvertY = false;
-    private CinemachineFreeLook _freeLookComponent;
+//[RequireComponent(typeof(CinemachineFreeLook))]
+//public class FreeLookControllerInput : MonoBehaviour
+//{
+//    [Range(0f, 10f)] public float LookSpeed = 1f;
+//    public bool InvertY = false;
+//    private CinemachineFreeLook _freeLookComponent;
 
-    private Vector2 camMoveInput;
+//    private Vector2 camMoveInput;
 
-    void Start()
-    {
-        _freeLookComponent = GetComponent<CinemachineFreeLook>();
-    }
+//    void Start()
+//    {
+//        _freeLookComponent = GetComponent<CinemachineFreeLook>();
+//    }
 
-    void Update()
-    {
-        if(Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt))
-        {
-            return;
-        }
-        //Normalize the vector to have an uniform vector in whichever form it came from (I.E Gamepad, mouse, etc)
-        Vector2 lookMovement = camMoveInput.normalized;
-        lookMovement.y = InvertY ? -lookMovement.y : lookMovement.y;
+//    void Update()
+//    {
+//        if(Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt))
+//        {
+//            return;
+//        }
+//        //Normalize the vector to have an uniform vector in whichever form it came from (I.E Gamepad, mouse, etc)
+//        Vector2 lookMovement = camMoveInput.normalized;
+//        lookMovement.y = InvertY ? -lookMovement.y : lookMovement.y;
 
-        // This is because X axis range is between -180 and 180 instead of 0 and 1 like the Y axis
-        lookMovement.x = lookMovement.x * 180f;
+//        // This is because X axis range is between -180 and 180 instead of 0 and 1 like the Y axis
+//        lookMovement.x = lookMovement.x * 180f;
 
-        //Ajust axis values using look speed and Time.deltaTime so the look doesn't go faster if there is more FPS
-        _freeLookComponent.m_XAxis.Value += lookMovement.x * LookSpeed * Time.deltaTime;
-        _freeLookComponent.m_YAxis.Value += lookMovement.y * LookSpeed * Time.deltaTime;
-    }
+//        //Ajust axis values using look speed and Time.deltaTime so the look doesn't go faster if there is more FPS
+//        _freeLookComponent.m_XAxis.Value += lookMovement.x * LookSpeed * Time.deltaTime;
+//        _freeLookComponent.m_YAxis.Value += lookMovement.y * LookSpeed * Time.deltaTime;
+//    }
 
-    // Update the look movement each time the event is trigger
-    public void OnLook(InputAction.CallbackContext context)
-    {
-        Debug.Log("Controller Look Axis Changed: " + context.ReadValue<Vector2>());
-        camMoveInput = context.ReadValue<Vector2>();
-    }
+//    // Update the look movement each time the event is trigger
+//    public void OnLook(InputAction.CallbackContext context)
+//    {
+//        Debug.Log("Controller Look Axis Changed: " + context.ReadValue<Vector2>());
+//        camMoveInput = context.ReadValue<Vector2>();
+//    }
 
-    public void OnLookMouse(InputAction.CallbackContext context)
-    {
-        Debug.Log("Mouse Look Axis Changed: " + context.ReadValue<Vector2>());
-        //Normalize the vector to have an uniform vector in whichever form it came from (I.E Gamepad, mouse, etc)
-        Vector2 lookMovement = context.ReadValue<Vector2>().normalized;
-        lookMovement.y = InvertY ? -lookMovement.y : lookMovement.y;
+//    public void OnLookMouse(InputAction.CallbackContext context)
+//    {
+//        Debug.Log("Mouse Look Axis Changed: " + context.ReadValue<Vector2>());
+//        //Normalize the vector to have an uniform vector in whichever form it came from (I.E Gamepad, mouse, etc)
+//        Vector2 lookMovement = context.ReadValue<Vector2>().normalized;
+//        lookMovement.y = InvertY ? -lookMovement.y : lookMovement.y;
 
-        // This is because X axis range is between -180 and 180 instead of 0 and 1 like the Y axis
-        lookMovement.x = lookMovement.x * 180f;
+//        // This is because X axis range is between -180 and 180 instead of 0 and 1 like the Y axis
+//        lookMovement.x = lookMovement.x * 180f;
 
-        //Ajust axis values using look speed and Time.deltaTime so the look doesn't go faster if there is more FPS
-        _freeLookComponent.m_XAxis.Value += lookMovement.x * LookSpeed * Time.deltaTime;
-        _freeLookComponent.m_YAxis.Value += lookMovement.y * LookSpeed * Time.deltaTime;
+//        //Ajust axis values using look speed and Time.deltaTime so the look doesn't go faster if there is more FPS
+//        _freeLookComponent.m_XAxis.Value += lookMovement.x * LookSpeed * Time.deltaTime;
+//        _freeLookComponent.m_YAxis.Value += lookMovement.y * LookSpeed * Time.deltaTime;
 
-    }
-}
+//    }
+//}
