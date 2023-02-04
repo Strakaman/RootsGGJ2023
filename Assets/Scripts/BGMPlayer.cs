04aa675b451d9249f0d4eeb70ec05929361f95ca
Omari Straker <straka3000@gmail.com>
2023-02-04 03:46:21 -0800

    some audio stuff, music, placeholder voice lines for testing, and a few sword sound fx

---
+++ Assets/Scripts/BGMPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGMPlayer : MonoBehaviour
+{
+    public AudioClip lobbyMusic;
+    public AudioClip[] matchMusic;
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+
+    }
+    void Start()
+    {
+        PlayASong();
+    }
+
+    void PlayASong()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            AudioManager.instance.PlayMusic(lobbyMusic);
+        }
+        else
+        {
+            AudioManager.instance.PlayMusic(matchMusic[UnityEngine.Random.Range(0, matchMusic.Length)]);
+        }
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        PlayASong();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void Start()
+    {
+        AudioManager.instance.PlaySound2D("MatchStart");
+    }
+}
